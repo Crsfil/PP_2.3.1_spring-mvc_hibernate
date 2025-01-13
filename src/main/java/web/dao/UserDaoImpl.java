621b7abe3e297cc93a2c1d672d147713e3a234cb
Crsfil <strizhovhotmail@gmail.com>
2025-01-13 23:59:52 +0300

    добавил аннотации, убрал лишнюю проверку

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,6 +1,7 @@
 package web.dao;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
 
 import javax.persistence.EntityManager;
@@ -12,31 +13,31 @@ public class UserDaoImpl implements UserDao {
     @PersistenceContext
     private EntityManager entityManager;
 
-
+    @Transactional
     @Override
     public void save(User user) {
-        if (user.getId() == null){
-            entityManager.persist(user);
-        } else {
-            entityManager.merge(user);
-        }
+        entityManager.persist(user);
     }
 
+    @Transactional
     @Override
     public void update(User user) {
         entityManager.merge(user);
     }
 
+    @Transactional
     @Override
     public void delete(User user) {
         entityManager.remove(user);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public User findById(Long id) {
         return entityManager.find(User.class, id);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<User> findAll() {
         return entityManager.createQuery("SELECT u FROM User u", User.class).getResultList();
